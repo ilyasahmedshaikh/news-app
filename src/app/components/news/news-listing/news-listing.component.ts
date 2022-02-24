0fefed46c814eb33a839d194069cf4980145993b
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2022-02-24 21:29:31 +0500

    news listing page done

--- src/app/components/news/news-listing/news-listing.component.ts
+++ src/app/components/news/news-listing/news-listing.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsListingComponent implements OnInit {
 
+  listing: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.listing = [
+      {
+        id: 1,
+        image: 'assets/img/1.png',
+        title: 'Breaking News | BBC News',
+        description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      },
+      {
+        id: 2,
+        image: 'assets/img/2.jpg',
+        title: 'Breaking News | Live News',
+        description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      },
+      {
+        id: 3,
+        image: 'assets/img/3.jpg',
+        title: 'Breaking News | Geo News',
+        description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      },
+      {
+        id: 4,
+        image: 'assets/img/4.jpg',
+        title: 'Breaking News | Express News',
+        description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      },
+      {
+        id: 5,
+        image: 'assets/img/5.png',
+        title: 'Breaking News | NBC News',
+        description: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      }
+    ]
   }
 
 }
